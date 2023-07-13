33e27b6bd3c94ba90b9c06e6b4042d48b9a8e71b
try <try418@163.com>
2023-07-13 17:58:48 +0800

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,15 +16,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://try.desing',
+  url: 'https://www.try.desing',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'try', // Usually your GitHub org/user name.
-  projectName: '备忘录', // Usually your repo name.
+  organizationName: 'Mr-try', // Usually your GitHub org/user name.
+  projectName: 'ten', // Usually your repo name.
+  // deploymentBranch:'',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
