5df9d9a92cd760162af74aae7866be5d0643224d
try <try418@163.com>
2023-07-14 18:47:57 +0800

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,34 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      docs: {
+        sidebar: {
+          // hideable: true,
+        },
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'YOUR_APP_ID',
+        // Public API key: it is safe to commit it
+        apiKey: 'YOUR_SEARCH_API_KEY',
+        indexName: 'YOUR_INDEX_NAME',
+        // Optional: see doc section below
+        contextualSearch: true,
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+        // Optional: Algolia search parameters
+        searchParameters: {},
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        // searchPagePath: 'search',
+        //... other Algolia params
+      },
       navbar: {
+        hideOnScroll: true,
         title: '',
         logo: {
           alt: 'try',
@@ -93,6 +120,10 @@ const config = {
             label: 'GitHub',
             position: 'right',
             logo: 'img/logo.png',
+          },
+          {
+            type: 'search',
+            position: 'right',
           },
         ],
       },
